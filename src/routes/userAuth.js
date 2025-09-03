7f7f57a028b7d6954b9397e1dd1bef6fcd994016
pksingh <pksingh69313@gmail.com>
2025-09-03 17:01:08 +0530

    Setup middleware

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { userSignup, userLogin } from "../controllers/userAuth.js";
+import { getProfile } from "../controllers/noteController.js";
+import authMiddleware from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin)
+router.get("/profile", authMiddleware, getProfile)
 
 export default router;
