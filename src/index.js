9ef14b6b84192410303169e1bccf8fac2a808412
pksingh <pksingh69313@gmail.com>
2025-09-06 03:57:19 +0530

    Added cors middleware

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ app.use(cors({
   origin: [
     'https://planitfirst.vercel.app',
     'http://localhost:5000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://localhost:3000'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
