4d6bc7108eb38f01d78fe2379bc02fe2cfe73e60
pksingh <pksingh69313@gmail.com>
2025-09-05 16:02:51 +0530

    Injected env variables

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,6 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { configDotenv } from "dotenv";
 
+dotenv.config()
 //cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
