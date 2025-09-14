53d10fda23e957449489f1a2e7603c39cba975d4
pksingh <pksingh69313@gmail.com>
2025-09-15 03:05:07 +0530

    fix: api mismatch and lost tokens

--- src/middleware/auth0Check.js
+++ src/middleware/auth0Check.js
@@ -3,19 +3,17 @@ import jwksRsa from "jwks-rsa";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-// Auth0 JWT verification
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_ISSUER_BASE_URL}/.well-known/jwks.json`
+    jwksUri: `https://${process.env.AUTH0_ISSUER_BASE_URL}/.well-known/jwks.json`,
   }),
   audience: process.env.AUTH0_AUDIENCE,
   issuer: `https://${process.env.AUTH0_ISSUER_BASE_URL}/`,
   algorithms: ["RS256"],
-  requestProperty: 'auth'
+  requestProperty: "auth",
 });
 
 export default checkJwt;
